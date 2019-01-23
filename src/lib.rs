d1eb135c709490081fe3a5000d9fcd967991eb2a
Daniel Stahl <danstahl1138@gmail.com>
2019-01-23 11:09:34 -0600

    added alpha stable distribution

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 extern crate num_complex;
 extern crate special;
 
+#[macro_use]
+#[cfg(test)]
+extern crate approx;
+
 use num_complex::Complex;
 use special::Gamma;
 
@@ -766,9 +770,174 @@ pub fn cgmy_diffusion_vol(
     ((sigma.powi(2)+c*(2.0-y).gamma()*(m.powf(y-2.0)+g.powf(y-2.0)))*maturity).sqrt()
 }
 
+
+fn runge_kutta_complex_vector(
+    fx: &Fn(f64, &Complex<f64>, &Complex<f64>)->(Complex<f64>, Complex<f64>), 
+    mut init_value_1:Complex<f64>, 
+    mut init_value_2:Complex<f64>, 
+    t: f64,
+    num_steps: usize
+)->(Complex<f64>, Complex<f64>){
+    let dt=t/(num_steps as f64);
+    let hfdt=dt*0.5;
+    let sixthdt=dt/6.0;
+    for index in 0..num_steps{
+        let t_curr=dt*(index as f64);
+        let (k11, k12)=fx(t_curr, &init_value_1, &init_value_2);
+        let (k21, k22)=fx(t_curr+hfdt, &(init_value_1+k11*hfdt), &(init_value_2+k12*hfdt));
+        let (k31, k32)=fx(t_curr+hfdt, &(init_value_1+k21*hfdt), &(init_value_2+k22*hfdt));
+        let (k41, k42)=fx(t_curr+dt, &(init_value_1+k21*dt), &(init_value_2+k22*dt));
+        init_value_1=init_value_1+(k11+2.0*k21+2.0*k31+k41)*sixthdt;
+        init_value_2=init_value_2+(k12+2.0*k22+2.0*k32+k42)*sixthdt;
+    }
+    (init_value_1, init_value_2)
+}
+
+//helper for ODE
+fn alpha_or_beta(rho:f64, k:f64, h:f64, l:f64)->impl (Fn(&Complex<f64>, &Complex<f64>)->Complex<f64>){
+    move |ode_val:&Complex<f64>, cf_val:&Complex<f64>|-rho+k*ode_val+0.5*ode_val*ode_val*h+l*cf_val
+}
+
+fn duffie_mgf_increment(
+    u: &Complex<f64>,
+    ode_val_1:&Complex<f64>, 
+    rho0:f64, rho1:f64, k0:f64, k1:f64, h0:f64, h1:f64, l0:f64, l1:f64, 
+    cf: &Fn(&Complex<f64>)->Complex<f64>
+)->(Complex<f64>, Complex<f64>){
+    let cf_part=cf(u)-1.0;
+    let alpha=alpha_or_beta(rho1, k1, h1, l1);
+    let beta=alpha_or_beta(rho0, k0, h0, l0);
+    (alpha(ode_val_1, &cf_part), beta(ode_val_1, &cf_part))
+}
+
+//jump leverage...http://web.stanford.edu/~duffie/dps.pdf page 10
+fn generic_leverage_jump(
+    u:&Complex<f64>,
+    cf: &Fn(&Complex<f64>)->Complex<f64>,
+    t:f64, v0:f64, correlation:f64, expected_value_of_cf:f64,
+    rho0:f64, rho1:f64, k0:f64, k1:f64, h0:f64, h1:f64, l0:f64, l1:f64,
+    num_steps:usize
+)->Complex<f64>{
+    let init_value_1=Complex::new(0.0, 0.0);
+    let init_value_2=Complex::new(0.0, 0.0);
+    let delta=if l1>0.0&& expected_value_of_cf>0.0{correlation/(expected_value_of_cf*l1)}else{0.0};
+    let fx=move |_t:f64, curr_val_1:&Complex<f64>, _curr_val_2:&Complex<f64>|{
+        duffie_mgf_increment(&(u+delta*curr_val_1), curr_val_1, rho0, rho1, k0, k1, h0, h1, l0, l1, cf) 
+    };
+    let (res1, res2)=runge_kutta_complex_vector(&fx, init_value_1, init_value_2, t, num_steps);
+    res1*v0+res2
+}
+
+const BETA_STABLE:f64=1.0;//for finite expectation
+//for alpha distribution
+fn alpha_stable_log(
+    u:&Complex<f64>, t:f64, v0:f64, a:f64, sigma:f64, lambda:f64, correlation:f64, 
+    alpha:f64, mu:f64, c:f64, num_steps:usize
+)->Complex<f64>{
+    let b=1.0-correlation;//to stay expectation of 1
+    generic_leverage_jump(
+        u, &|u|stable_cf(u, alpha, mu, BETA_STABLE, c), t, v0, correlation, mu,
+        0.0, 0.0, a*b, -a, 0.0, sigma.powi(2), 0.0, lambda, num_steps
+    )
+}
+
+/// Returns log CF of an alpha stable process when transformed by an affine process
+/// and the process is correlated with the jump component of the Levy process.
+/// 
+/// # Remarks
+/// The time change is assumed to be a single-dimensional CIR process with a 
+/// jump component with mean 1.  
+/// The correlation between the Levy process and the affine process is due
+/// to sharing the same jumps (both the Levy process and the affine process
+/// jump at the same time).
+/// 
+/// # Examples
+/// 
+/// ```
+/// extern crate num_complex;
+/// use num_complex::Complex;
+/// extern crate cf_functions;
+/// # fn main() {
+/// let mu=1300.0;
+/// let c=100.0;
+/// let alpha=1.1;
+/// let lambda=100.0;
+/// let correlation=0.9;
+/// let a=0.4;
+/// let sigma=0.4;
+/// let t=1.0;
+/// let v0=1.0;
+/// let num_steps:usize=1024;
+/// let cf=|u:&Complex<f64>|u.exp();
+/// let cf=cf_functions::alpha_stable_leverage(
+///     t, v0, a, sigma, lambda, 
+///     correlation, alpha, mu, c, num_steps
+/// );
+/// let u=Complex::new(0.05, -0.5);
+/// let value_of_cf=cf(&u);
+/// # }
+/// ```
+pub fn alpha_stable_leverage(
+    t:f64, v0:f64, a:f64, sigma:f64, lambda:f64, correlation:f64, 
+    alpha:f64, mu:f64, c:f64, num_steps:usize
+)->impl Fn(&Complex<f64>)->Complex<f64>{
+    move |u|{
+        alpha_stable_log(
+            u, t, v0, a, sigma, lambda, correlation, 
+            alpha, mu, c, num_steps
+        ).exp()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn runge_kutta(){
+        let t=2.0;
+        let num_steps=1024;
+        let init_value_1=Complex::new(1.0, 0.0);
+        let init_value_2=Complex::new(1.0, 0.0);
+        let (res1, res2)=runge_kutta_complex_vector(
+            &|t:f64, val1: &Complex<f64>, val2: &Complex<f64>|(val1*t, val2*t),
+            init_value_1, init_value_2, t, num_steps
+        );
+        assert_abs_diff_eq!(res1.re, (2.0 as f64).exp(), epsilon=0.00001);
+        assert_abs_diff_eq!(res2.re, (2.0 as f64).exp(), epsilon=0.00001);
+    }
+    #[test]
+    fn duffie_mgf_compare_cir(){
+        let sigma=0.3;
+        let a=0.3;
+        let b=0.05;
+        let r0=0.05;
+        let h=(a*a+2.0*sigma*sigma as f64).sqrt();
+        let t=0.25;
+        let a_num=2.0*h*((a+h)*t*0.5).exp();
+        let a_den=2.0*h+(a+h)*((t*h).exp()-1.0);
+        let a_t_tm=(a_num/a_den).powf(2.0*a*b/(sigma*sigma));
+        let b_num=2.0*((t*h).exp()-1.0);
+        let b_den=a_den;
+        let b_t_tm=b_num/b_den;
+        let bond_price=a_t_tm*((-r0*b_t_tm).exp());
+        let rho0=0.0;
+        let rho1=1.0;
+        let k0=a*b;
+        let k1=-a;
+        let h0=0.0;
+        let h1=sigma*sigma;
+        let l0=0.0;
+        let l1=0.0;
+        let num_steps:usize=1024;
+        let cf=|u:&Complex<f64>|u.exp();
+        let correlation=0.0;
+        let expected_value_of_cf=1.0;//doesnt matter
+        let u=Complex::new(1.0, 0.0);
+        let result=generic_leverage_jump(&u, &cf,
+            t, r0, correlation, expected_value_of_cf,
+            rho0, rho1, k0, k1, h0, h1, l0, l1, num_steps);
+        assert_abs_diff_eq!(result.re.exp(), bond_price, epsilon=0.000001);
+    }
     #[test]
     fn cir_analytical() {
         let sigma=0.3;
