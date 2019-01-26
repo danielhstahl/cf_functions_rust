5e6ce8cd5ec4db22189858b1b1985bb82e0dfb33
Daniel Stahl <danstahl1138@gmail.com>
2019-01-26 08:30:28 -0600

    added more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1014,6 +1014,44 @@ mod tests {
         assert_abs_diff_eq!(expectation, lambda*alpha*beta*t, epsilon=0.00001);
     }
     #[test]
+    fn test_gamma_leverage_expectation_leq(){
+        let t=2.0;
+        let num_steps=1024;
+        let v0=0.9;
+        let a=0.4;
+        let sigma=0.4;
+        let lambda=100.0;
+        let correlation=0.9;
+        let alpha=2.0;
+        let beta=4.0;
+        let x_min=0.0;
+        let x_max=lambda*alpha*beta*beta*5.0*t;
+        let num_u:usize=256;
+        let cf=gamma_leverage(t, v0, a, sigma, lambda, correlation, alpha, beta, num_steps);
+        let discrete_cf=fang_oost::get_discrete_cf(num_u, x_min, x_max, &cf);
+        let expectation=cf_dist_utils::get_expectation_discrete_cf(x_min, x_max, &discrete_cf);
+        assert_eq!(expectation<lambda*alpha*beta*t, true);
+    }
+    #[test]
+    fn test_gamma_leverage_expectation_geq(){
+        let t=2.0;
+        let num_steps=1024;
+        let v0=1.1;
+        let a=0.4;
+        let sigma=0.4;
+        let lambda=100.0;
+        let correlation=0.9;
+        let alpha=2.0;
+        let beta=4.0;
+        let x_min=0.0;
+        let x_max=lambda*alpha*beta*beta*5.0*t;
+        let num_u:usize=256;
+        let cf=gamma_leverage(t, v0, a, sigma, lambda, correlation, alpha, beta, num_steps);
+        let discrete_cf=fang_oost::get_discrete_cf(num_u, x_min, x_max, &cf);
+        let expectation=cf_dist_utils::get_expectation_discrete_cf(x_min, x_max, &discrete_cf);
+        assert_eq!(expectation>lambda*alpha*beta*t, true);
+    }
+    #[test]
     fn runge_kutta(){
         let t=2.0;
         let num_steps=1024;
