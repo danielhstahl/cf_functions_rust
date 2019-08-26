65933f3204713e871bfd3c72404a7c8cd7ab0621
Daniel Stahl <danstahl1138@gmail.com>
2019-08-26 12:55:30 +0000

    compared numeric and analytical mgf

---
+++ benches/mgf_compare.rs
@@ -0,0 +1,48 @@
+#[macro_use]
+extern crate criterion;
+extern crate num_complex;
+extern crate cf_functions;
+use criterion::{Criterion, ParameterizedBenchmark};
+use num_complex::{Complex};
+
+fn bench_mgf(c: &mut Criterion) {
+    c.bench("cir_mgf_analytical_numerical",
+        ParameterizedBenchmark::new("analytical", |b, _i|{
+            b.iter(|| {
+                let sigma=0.3;
+                let a=0.3;
+                let b=0.05;
+                let r0=0.05;
+                let t=0.25;
+                cf_functions::cir_mgf(&Complex::new(1.0, 0.0), a*b, a, sigma, t, r0).re
+            });
+        }, vec![128, 256, 512, 1024]).with_function("numeric",|b, i|{
+            b.iter(|| {
+                let sigma=0.3;
+                let a=0.3;
+                let b=0.05;
+                let r0=0.05;
+                //let h=(a*a+2.0*sigma*sigma as f64).sqrt();
+                let t=0.25;
+                let rho0=0.0;
+                let rho1=1.0;
+                let k0=a*b;
+                let k1=-a;
+                let h0=0.0;
+                let h1=sigma*sigma;
+                let l0=0.0;
+                let l1=0.0;
+                let cf=|u:&Complex<f64>|u.exp();
+                let correlation=0.0;
+                let expected_value_of_cf=1.0;//doesnt matter
+                let u=Complex::new(1.0, 0.0);
+                cf_functions::generic_leverage_jump(&u, &cf,
+                    t, r0, correlation, expected_value_of_cf,
+                    rho0, rho1, k0, k1, h0, h1, l0, l1, *i).re
+            });
+        })
+    );
+}
+
+criterion_group!(benches, bench_mgf);
+criterion_main!(benches);
