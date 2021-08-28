35b3ab9bdf2bd013dfadaf1343b70e0b41e6d208
Daniel Stahl <danstahl1138@gmail.com>
2021-08-28 10:49:06 -0500

    Added test

--- src/cgmy.rs
+++ src/cgmy.rs
@@ -282,115 +282,21 @@ pub fn cgmy_diffusion_vol(sigma: f64, c: f64, g: f64, m: f64, y: f64, maturity: 
 mod tests {
     use super::*;
     use approx::*;
+    //use cf_dist_utils::*;
     #[test]
     fn cgmy_expectation() {
         let t = 2.0;
         let num_steps = 1024;
-        let init_value_1 = Complex::new(1.0, 0.0);
-        let init_value_2 = Complex::new(1.0, 0.0);
-        let (res1, res2) = runge_kutta_complex_vector(
-            &|t: f64, val1: &Complex<f64>, val2: &Complex<f64>| (val1 * t, val2 * t),
-            init_value_1,
-            init_value_2,
-            t,
-            num_steps,
-        );
-        assert_abs_diff_eq!(res1.re, (2.0 as f64).exp(), epsilon = 0.00001);
-        assert_abs_diff_eq!(res2.re, (2.0 as f64).exp(), epsilon = 0.00001);
-    }
-    #[test]
-    fn duffie_mgf_compare_cir() {
-        let sigma = 0.3;
-        let a = 0.3;
-        let b = 0.05;
-        let r0 = 0.05;
-        let h = (a * a + 2.0 * sigma * sigma as f64).sqrt();
-        let t = 0.25;
-        let a_num = 2.0 * h * ((a + h) * t * 0.5).exp();
-        let a_den = 2.0 * h + (a + h) * ((t * h).exp() - 1.0);
-        let a_t_tm = (a_num / a_den).powf(2.0 * a * b / (sigma * sigma));
-        let b_num = 2.0 * ((t * h).exp() - 1.0);
-        let b_den = a_den;
-        let b_t_tm = b_num / b_den;
-        let bond_price = a_t_tm * ((-r0 * b_t_tm).exp());
-        let rho0 = 0.0;
-        let rho1 = 1.0;
-        let k0 = a * b;
-        let k1 = -a;
-        let h0 = 0.0;
-        let h1 = sigma * sigma;
-        let l0 = 0.0;
-        let l1 = 0.0;
-        let num_steps: usize = 1024;
-        let cf = |u: &Complex<f64>| u.exp();
-        let correlation = 0.0;
-        let expected_value_of_cf = 1.0; //doesnt matter
-        let u = Complex::new(1.0, 0.0);
-        let result = generic_leverage_jump(
-            &u,
-            &cf,
-            t,
-            r0,
-            correlation,
-            expected_value_of_cf,
-            rho0,
-            rho1,
-            k0,
-            k1,
-            h0,
-            h1,
-            l0,
-            l1,
-            num_steps,
-        );
-        assert_abs_diff_eq!(result.re.exp(), bond_price, epsilon = 0.000001);
-    }
-    #[test]
-    fn cir_analytical() {
-        let sigma = 0.3;
-        let a = 0.3;
-        let b = 0.05;
-        let r0 = 0.05;
-        let h = (a * a + 2.0 * sigma * sigma as f64).sqrt();
-        let t = 0.25;
-        let a_num = 2.0 * h * ((a + h) * t * 0.5).exp();
-        let a_den = 2.0 * h + (a + h) * ((t * h).exp() - 1.0);
-        let a_t_tm = (a_num / a_den).powf(2.0 * a * b / (sigma * sigma));
-        let b_num = 2.0 * ((t * h).exp() - 1.0);
-        let b_den = a_den;
-        let b_t_tm = b_num / b_den;
-        let bond_price = a_t_tm * ((-r0 * b_t_tm).exp());
-        assert_eq!(
-            bond_price,
-            cir_mgf(&Complex::new(1.0, 0.0), a * b, a, sigma, t, r0).re
-        );
-    }
-    #[test]
-    fn cir_with_zeros() {
-        let t = 1.0;
-        let r0 = 0.04;
-        let approx_bond_price = cir_mgf(&Complex::new(1.0, 0.0), 0.0, 0.0, 0.0, t, r0).re;
-        assert_eq!(approx_bond_price.is_nan(), false);
-    }
-    #[test]
-    fn cir_heston() {
-        let t = 0.25;
-        let k = 0.2;
-        let v0 = 0.98;
-        let sig = 0.2;
-        let rho = -0.3;
-        let sig_tot = 0.3;
-        let u = Complex::new(0.5, 0.5);
-        let neg_psi = 0.5 * sig_tot * sig_tot * (u - u * u);
-        let k_star = k - u * rho * sig * sig_tot;
-        let ada = (k_star * k_star + 2.0 * sig * sig * neg_psi as Complex<f64>).sqrt();
-        let b_t = 2.0 * neg_psi * (1.0 - (-ada * t).exp())
-            / (2.0 * ada - (ada - k_star) * (1.0 - (-ada * t).exp()));
-        let c_t = (k / (sig * sig))
-            * (2.0 * (1.0 - (1.0 - (-ada * t).exp()) * (ada - k_star) / (2.0 * ada)).ln()
-                + (ada - k_star) * t);
-        let cf_heston = (-b_t * v0 - c_t).exp().re;
-        let approx_heston_cf = cir_mgf_cmp(&neg_psi, k, &k_star, sig, t, v0).re;
-        assert_eq!(cf_heston, approx_heston_cf);
+        let x_min = -10.0;
+        let x_max = 10.0;
+        let c = 0.5;
+        let g = 2.0;
+        let y = 1.2;
+        let cf = |u: &Complex<f64>| cgmy_log_cf(&u, c, g, 0.0, y).exp();
+        let discrete_cf = fang_oost::get_discrete_cf(num_steps, x_min, x_max, &cf);
+        let approx_expectation =
+            cf_dist_utils::get_expectation_discrete_cf(x_min, x_max, &discrete_cf);
+        let expectation = cgmy_negative_jump_expectation(c, g, y);
+        assert_abs_diff_eq!(expectation, approx_expectation, epsilon = 0.00001);
     }
 }
